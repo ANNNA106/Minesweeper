cb04d96e362a12c50ee9168f9844bf2bcb4877f6
ANNNA106 <ananyabaghel2004@gmail.com>
2024-08-06 21:11:06 +0530

    added flag

---
+++ script.js
@@ -0,0 +1,87 @@
+let board = [];
+let rows = 8;
+let columns = 8;
+
+let minesCount = 10;
+let minesLocation = []; // "2-2", "3-4", "2-1"
+
+let tilesClicked = 0; //goal to click all tiles except the ones containing mines
+let flagEnabled = false;
+
+let gameOver = false;
+
+window.onload = function() {
+    startGame();
+}
+
+function setMines() {
+    // minesLocation.push("2-2");
+    // minesLocation.push("2-3");
+    // minesLocation.push("5-6");
+    // minesLocation.push("3-4");
+    // minesLocation.push("1-1");
+
+    let minesLeft = minesCount;
+    while (minesLeft > 0) { 
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        let id = r.toString() + "-" + c.toString();
+
+        if (!minesLocation.includes(id)) {
+            minesLocation.push(id);
+            minesLeft -= 1;
+        }
+    }
+}
+
+
+function startGame() {
+    document.getElementById("mines-count").innerText = minesCount;
+    document.getElementById("flag-button").addEventListener("click", setFlag);
+    setMines();
+
+    //populate our board
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            //<div id="0-0"></div>
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.addEventListener("click", clickTile);
+            document.getElementById("board").append(tile);
+            row.push(tile);
+        }
+        board.push(row);
+    }
+
+    console.log(board);
+}
+
+function setFlag() {
+    if (flagEnabled) {
+        flagEnabled = false;
+        document.getElementById("flag-button").style.backgroundColor = "lightgray";
+    }
+    else {
+        flagEnabled = true;
+        document.getElementById("flag-button").style.backgroundColor = "darkgray";
+    }
+}
+
+function clickTile() {
+    if (gameOver || this.classList.contains("tile-clicked")) {
+        return;
+    }
+
+    let tile = this;
+    if (flagEnabled) {
+        if (tile.innerText == "") {
+            tile.innerText = "🚩";
+        }
+        else if (tile.innerText == "🚩") {
+            tile.innerText = "";
+        }
+        return;
+    }
+
+}
