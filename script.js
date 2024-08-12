92cf6ffad150072de12ed637382d163af26ec585
Anya Rajan <135256087+AnyaRajan@users.noreply.github.com>
2024-08-12 13:01:06 +0530

    added the main menu page and the how to play page

--- script.js
+++ script.js
@@ -85,3 +85,24 @@ function clickTile() {
     }
 
 }
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('rules-modal');
+    const howToPlayBtn = document.getElementById('how-to-play-btn');
+    const closeBtn = document.querySelector('#rules-content .close');
+
+    howToPlayBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        modal.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
+
